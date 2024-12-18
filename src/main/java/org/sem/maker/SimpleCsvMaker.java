2e26ffd4694a1729e1f010eb49856677fedb6c7f
gooinsung <gooinsung96@gmail.com>
2024-12-18 22:12:17 +0900

    SimpleCsvMaker.java

--- src/main/java/org/sem/maker/SimpleCsvMaker.java
+++ src/main/java/org/sem/maker/SimpleCsvMaker.java
@@ -1,21 +1,46 @@
 package org.sem.maker;
 
 import org.sem.data.SemInput;
+import org.sem.util.SemUtils;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SimpleCsvMaker implements SimpleMaker {
-    private final String DEFAULT_FILE_NAME = "excel.csv";
 
     @Override
     public void make(SemInput input) {
+        List<?> dataList = input.getDataList();
 
+        Field[] fields = SemUtils.getFields(dataList);
+
+        List<String> keyList = getKeyList(fields);
+
+        try (FileWriter writer = new FileWriter(input.getFileFullName())) {
+            writer.append(String.join(",", keyList));
+            writer.append("\n");
+
+            for (Object item : dataList) {
+                List<String> row = new ArrayList<>();
+                for (Field field : fields) {
+                    field.setAccessible(Boolean.TRUE);
+                    row.add(field.get(item).toString());
+                }
+                writer.append(String.join(",", row));
+                writer.append("\n");
+            }
+        } catch (IOException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<String> getKeyList(Field[] fields) {
-        return null;
+        return Arrays.stream(fields).map(Field::getName).toList();
     }
 
 }
--- src/test/java/maker/SimpleCsvMakerTest.java
+++ src/test/java/maker/SimpleCsvMakerTest.java
@@ -3,6 +3,8 @@ package maker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sem.data.SemInput;
+import org.sem.enums.ExtensionType;
+import org.sem.maker.SimpleCsvMaker;
 import org.sem.maker.SimpleExcelMaker;
 import org.sem.maker.SimpleMaker;
 import test.User;
@@ -10,8 +12,8 @@ import test.User;
 import java.io.File;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-class SimpleExcelMakerTest {
+import static org.junit.jupiter.api.Assertions.*;
+class SimpleCsvMakerTest {
 
     @Test
     @DisplayName("Test make")
@@ -26,9 +28,10 @@ class SimpleExcelMakerTest {
                 .fileName(fileName)
                 .sheetName(sheetName)
                 .dataList(userList)
+                .extensionType(ExtensionType.CSV)
                 .build();
         // when
-        SimpleMaker maker = new SimpleExcelMaker();
+        SimpleMaker maker = new SimpleCsvMaker();
         maker.make(input);
         // then
         File file = new File(input.getFileFullName());
