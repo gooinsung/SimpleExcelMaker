132743e6d2e883d99ab820db2e515b6874391a76
gooinsung <gooinsung96@gmail.com>
2024-12-18 22:51:02 +0900

    examples

---
+++ src/main/java/example/User.java
@@ -0,0 +1,15 @@
+package example;
+
+public class User {
+    private String name;
+    private int age;
+    private String address;
+    private String phoneNumber;
+
+    public User(String name, int age, String address, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
